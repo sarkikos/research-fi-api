8e7a38b9f072bc56ae97623af180b6f1d12a3be0
Timo Särkikoski <timo.sarkikoski@visma.com>
2019-04-12 11:25:21 +0300

    Return Http status 502 Bad Gateway when there is no connection to Elasticsearch host

--- django/webapps/portalapi/views.py
+++ django/webapps/portalapi/views.py
@@ -4,8 +4,9 @@
 #
 # :author: CSC - IT Center for Science Ltd., Espoo Finland servicedesk@csc.fi
 # :license: MIT
-from django.http import HttpResponseNotAllowed
+from django.http import HttpResponseNotAllowed, HttpResponse
 from django.conf import settings
+from http import HTTPStatus
 from revproxy.views import ProxyView
 
 
@@ -21,13 +22,18 @@ class PublicationProxyView(ProxyView):
         """
         Extend ProxyView dispatch method.
         Check that type of HTTP request is GET.
+        Return HTTP status 502 Bad Gateway in case connection to Elasticsearch fails.
         """
         if request.method != 'GET':
             # Return 405 Method Not Allowed.
             # Argument to the constructor is a list of permitted methods.
             return HttpResponseNotAllowed(['GET'])
 
-        return super(PublicationProxyView, self).dispatch(request, *args, **kwargs)
+        try:
+            return super(PublicationProxyView, self).dispatch(request, *args, **kwargs)
+        except:
+            print('Error: Cannot connect to Elasticsearch at ' + settings.ELASTICSEARCH_HOST)
+            return HttpResponse(status=HTTPStatus.BAD_GATEWAY.value)
 
     # The Content-Type that will be added to the response in case the upstream server doesn’t send it
     default_content_type = 'application/json'
