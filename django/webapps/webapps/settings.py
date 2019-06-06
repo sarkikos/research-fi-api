374a55d6c0b11e5725ddd47cbbf4ad3308163af0
Timo Särkikoski <sarkikos@gmail.com>
2019-06-06 12:10:12 +0300

    Added S2I configuration file for OpenShift. Removed SQLite database definition.

--- django/webapps/webapps/settings.py
+++ django/webapps/webapps/settings.py
@@ -78,10 +78,10 @@ WSGI_APPLICATION = 'webapps.wsgi.application'
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#    }
 }
 
 # Password validation
